e405949dd7e4c01d7b802b23eecd1e354845888e
jguo <jiaxuan_guo@163.com>
2024-04-10 16:31:29 +0800

    chore: change names

--- packages/calculator/src/index.ts
+++ packages/calculator/src/index.ts
@@ -13,17 +13,17 @@ export const getVDOT = (seconds: number, meters: number): number => {
   return Formula.getVDOT(meters, secondsToMinutes(seconds))
 }
 
-type RacePaces = Record<RaceType, Time>
-export const getEquivalentRacePacesFromVdot = (vdot: number): RacePaces => {
+type RaceTimes = Record<RaceType, Time>
+export const getEquivalentRaceTimesFromVdot = (vdot: number): RaceTimes => {
   return Object.fromEntries(RACE_TYPES
     .map(type => {
       const distance = RACE_DISTANCES[type]
       const timeInMinutes = Formula.getPredictedRaceTime(vdot, distance)
       return [type, secondsToTime(minutesToSeconds(timeInMinutes))]
-    })) as RacePaces
+    })) as RaceTimes
 }
-export const getEquivalentRacePaces = (seconds: number, meters: number): RacePaces => {
+export const getEquivalentRaceTimes = (seconds: number, meters: number): RaceTimes => {
   const vdot = getVDOT(seconds, meters)
-  return getEquivalentRacePacesFromVdot(vdot)
+  return getEquivalentRaceTimesFromVdot(vdot)
 }
 
