e2a748d317f009dc29bbed4eabad0c468171bdb6
jguo <jiaxuan_guo@163.com>
2024-04-10 15:57:19 +0800

    feat: RACE_TYPES, RACE_DISTANCES

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -34,3 +34,15 @@ const paceToSpeed = (pace: Time): number => {
   const secondsPerKilometer = timeToSeconds(pace)
   return METERS_IN_KILOMETER / secondsPerKilometer
 }
+
+type ArrayElementType<ArrayType> = ArrayType extends ReadonlyArray<infer T> ? T : never
+export const RACE_TYPES = ['marathon', 'half', '10k', '5k', '3k', '1500'] as const
+export type RaceType = ArrayElementType<typeof RACE_TYPES>
+export const RACE_DISTANCES: Record<RaceType, number> = {
+  marathon: 41195,
+  half: 21097,
+  '10k': 10000,
+  '5k': 5000,
+  '3k': 3000,
+  '1500': 1500,
+}
